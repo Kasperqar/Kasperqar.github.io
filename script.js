57945fa4614c103a65306eb056843c05da07073d
Kasperqar <159633468+Kasperqar@users.noreply.github.com>
2024-09-07 14:22:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Zmieniamy selektor, aby działał z obrazem "ja".
-    const images = document.querySelectorAll(".ja");
-
+    const images = document.querySelectorAll(".doCV");
+  
     images.forEach(image => {
-        image.addEventListener("mouseenter", function () {
-            // Wpierw szary dla wszystkich innych obrazów
-            images.forEach(otherImage => {
-                if (otherImage !== image) {
-                    otherImage.style.filter = "grayscale(1)"; // Szary dla innych
-                    otherImage.style.transform = "scale(1)";  // Przywrócenie skali
-                    otherImage.style.zIndex = 2;              // Zmiana z-index na niższy
-                }
-            });
-
-            // Dla aktualnego obrazu
-            image.style.filter = "grayscale(0)"; // Kolor dla wybranego
-            image.style.transform = "scale(1.4)"; // Powiększenie
-            image.style.zIndex = 4;               // Wyższy z-index, aby był na wierzchu
+      image.addEventListener("mouseenter", function () {
+        images.forEach(otherImage => {
+          if (otherImage !== image) {
+            otherImage.style.filter = "grayscale(1)";
+            otherImage.style.transform = "scale(1)";
+          }
         });
-
-        image.addEventListener("mouseleave", function () {
-            // Przywracanie efektu oryginalnego dla wszystkich
-            images.forEach(otherImage => {
-                otherImage.style.filter = "grayscale(0)"; // Przywrócenie koloru
-                otherImage.style.transform = "scale(1)";  // Przywrócenie skali
-                otherImage.style.zIndex = 3;              // Przywrócenie pierwotnego z-indexu
-            });
+  
+        image.style.filter = "grayscale(0)";
+        image.style.transform = "scale(1.4)";
+        image.style.zIndex = 4; // Ustawienie wyższego indeksu z warstwy, aby zdjęcie było na wierzchu
+      });
+  
+      image.addEventListener("mouseleave", function () {
+        images.forEach(otherImage => {
+          otherImage.style.filter = "grayscale(0)";
+          otherImage.style.transform = "scale(1)";
+          otherImage.style.zIndex = 3; // Przywrócenie pierwotnego indeksu z warstwy
         });
+      });
     });
-});
+  });
